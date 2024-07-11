210e453edd1a8ac320a0d817ef09c30cf28cebd5
Noah Silva <noahs9576@gmail.com>
2024-07-11 17:59:41 -0400

    ok im rly finished this time

--- p1_3221/part_1/fcfs.c
+++ p1_3221/part_1/fcfs.c
@@ -6,15 +6,19 @@ EECS Login ID (the one you use to access the red server): noahs957
 YorkU email address (the one that appears in eClass): noahs957@my.yorku.ca
 */
 
+// Header files
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "sch-helpers.h"  
 
+// Global variables
 process processes[MAX_PROCESSES+1];   
 int numberOfProcesses=0;              
 
  int main(int argc, char *argv[]) {
+
+    // Variables needed for calculations or printing
     int status;
     int currentTime=0;
     int contextSwitches=0;
@@ -29,35 +33,45 @@ int numberOfProcesses=0;
 
     qsort(processes, numberOfProcesses, sizeof(process), compareByArrival);
 
+    // Loop through all processes
     for (int i = 0; i < numberOfProcesses; i++) {
+        // Get the current process and set its pointer to the current process address
         process *p = &processes[i];
         int turnaroundTime = 0;
 
+        // If the current time is less than the arrival time of the process set the current time to the arrival time
         if (currentTime < p->arrivalTime) {
             currentTime = p->arrivalTime;
         }
 
+        // Set the start time and waiting time of the process
         p->startTime = currentTime;
         p->waitingTime = currentTime - p->arrivalTime;
 
+        // Loop through all the bursts of the process
         for (int j = 0; j < p->numberOfBursts; j++) {
             currentTime += p->bursts[j].length;
         }
 
+        // Set the end time of the process and calculate the turnaround time
         p->endTime = currentTime;
         turnaroundTime = p->endTime - p->arrivalTime;
 
+        // Add the waiting time and turnaround time to the total waiting and turnaround times
         totalWaitTime += p->waitingTime;
         totalTurnaroundTime += turnaroundTime;
 
+        // Store each process's pid in the last process variable so the last one overwrites the rest
         lastProcess = p->pid;
 
     }
 
+    // Calculations
     double avgWaitTime = totalWaitTime / numberOfProcesses;
     double avgTurnaroundTime = totalTurnaroundTime / numberOfProcesses;
     double cpuUtilization = (double)currentTime / (currentTime + totalWaitTime) * 100;
 
+    // Printing the necessary information
     fprintf(stdout, "Avg. Waiting Time: %.2f\n", avgWaitTime);
     fprintf(stdout, "Avg. Turnaround Time: %.2f\n", avgTurnaroundTime);
     fprintf(stdout, "Total Time: %d\n", currentTime);
